841ce1f847cebec3b4ebc10ba3a1b605ace0dc20
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-01 14:36:41 -0500

    Finished day1 both parts. I should use classy prelude

---
+++ haskell/src/y2017/Day01.hs
@@ -0,0 +1,33 @@
+module Day01 where
+
+import qualified Data.Text.IO as TIO
+import qualified Data.Text    as T
+
+import           Data.Char
+
+
+
+main :: IO ()
+main = do
+  input <- TIO.readFile "src/y2017/input01"
+  let processed = map digitToInt $ filter isNumber $ T.unpack input
+      p1 = part1 processed
+      p2 = part2 processed
+  TIO.putStrLn $ T.pack $ show p1
+  TIO.putStrLn $ T.pack $ show p2
+  return ()
+
+part1 :: [Int] -> Int
+part1 nums = walk (head nums) nums
+  where
+    walk :: Int -> [Int] -> Int
+    walk f []     = error "Empty list"
+    walk f (a:[]) = if a == f then f else 0
+    walk f (a:as) = if a == head as then a + walk f as
+                                    else     walk f as
+
+
+part2 :: [Int] -> Int
+part2 nums = sum $ zipWith (\a b -> if a == b then a else 0) nums (drop l nums ++ take l nums)
+  where
+    l = length nums `div` 2
