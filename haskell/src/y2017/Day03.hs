065194f1495162855b29f226efa24addc6d7c13e
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-03 16:07:23 -0500

    Simplify solution so much it's crazy

--- haskell/src/y2017/Day03.hs
+++ haskell/src/y2017/Day03.hs
@@ -16,11 +16,8 @@ part1 :: Int -> Int
 part1 p = if p == 1 then 0 else walk 1
   where
     walk i = if lr (i-1) < p && p <= lr i
-             then trace ("a"++show i) $ 2*i - ((u i - ur i) - abs( (p - ur i) `mod` (ul i - ur i) - (u i - ur i) ))
-             else trace ("b"++show i) $ walk (i+1)
+             then i + abs((p - ur i) `mod` i)
+             else (walk (i+1))
 
-ul n = 4*n^2       + 1
-u  n = 4*n^2 - 1*n + 1
 ur n = 4*n^2 - 2*n + 1
-r  n = 4*n^2 - 3*n + 1
 lr n = 4*n^2 + 4*n + 1
