73f3c4e47a6335b7730dc428c9cfdfb59211208f
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-03 16:29:57 -0500

    Simplfy completely!

--- haskell/src/y2017/Day03.hs
+++ haskell/src/y2017/Day03.hs
@@ -1,23 +1,18 @@
 module Day03 where
 
-import           Data.Text (Text)
-import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
-
-import Debug.Trace
 
 tprint :: Show a => a -> IO ()
 tprint = TIO.putStrLn . T.pack . show
 
-main = do
-  tprint $ part1 289326
+--main = do
+--  tprint $ part1 289326
 
 part1 :: Int -> Int
-part1 p = if p == 1 then 0 else walk 1
+part1 1 = 0
+part1 p = shell + abs((p - (4*shell^2-2*shell+1)) `mod` shell)
   where
-    walk i = if lr (i-1) < p && p <= lr i
-             then i + abs((p - ur i) `mod` i)
-             else (walk (i+1))
+    shell = ceiling $ (sqrt (fromIntegral n) - 1) / 2
+    n = fromIntegral p
 
-ur n = 4*n^2 - 2*n + 1
-lr n = 4*n^2 + 4*n + 1
+
