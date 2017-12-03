2fc12d3b89f042b9562e318bf5d6bec9789e4f8d
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-03 15:54:30 -0500

    Finish part 1. Had lots of trouble though

--- haskell/src/y2017/Day03.hs
+++ haskell/src/y2017/Day03.hs
@@ -15,10 +15,9 @@ main = do
 part1 :: Int -> Int
 part1 p = if p == 1 then 0 else walk 1
   where
-    walk i = case (lr (i-1)+1 <= p && p <= ur i,   ur i <= p && p <= lr i) of
-               (True, _) -> trace ("1"++show i) $ 2*i - abs(p - ur i)
-               (_, True) -> trace ("2"++show i) $ 2*i - abs( (p - ur i) `mod` (ul i - ur i) - (u i - ur i) )
-               (_,    _) -> trace ("3"++show i) $ walk (i+1)
+    walk i = if lr (i-1) < p && p <= lr i
+             then trace ("a"++show i) $ 2*i - ((u i - ur i) - abs( (p - ur i) `mod` (ul i - ur i) - (u i - ur i) ))
+             else trace ("b"++show i) $ walk (i+1)
 
 ul n = 4*n^2       + 1
 u  n = 4*n^2 - 1*n + 1
