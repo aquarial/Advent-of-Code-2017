995f8f1b50b6844bf60e2ca3995684a981bef32e
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-03 21:11:49 -0500

    Solve part2. Still kinda ugly. realign imports.

--- haskell/src/y2017/Day03.hs
+++ haskell/src/y2017/Day03.hs
@@ -1,13 +1,18 @@
 module Day03 where
 
-import qualified Data.Text.IO as TIO
-import qualified Data.Text    as T
+import qualified Data.Text           as T
+import qualified Data.Text.IO        as TIO
+
+import           Data.Maybe          (catMaybes)
+import           Data.List           (scanl')
+import qualified Data.HashMap.Strict as HMap
 
 tprint :: Show a => a -> IO ()
 tprint = TIO.putStrLn . T.pack . show
 
 main = do
   tprint $ part1 289326
+  tprint $ part2 289326
 
 part1 :: Int -> Int
 part1 1 = 0
@@ -17,3 +22,36 @@ part1 p = layer + abs((p - (4*layer^2 - 2*layer + 1)) `mod` layer)
     n = fromIntegral p
 
 
+
+
+part2 :: Int -> Int
+part2 n = firstGood $ map (\(p,m) -> HMap.lookup p m) $ zip spiral $ drop 1 $ scanl' addSquare initMap spiral
+  where
+    initMap = HMap.fromList [((0,0), 1)]
+
+    firstGood (Nothing:xs) = firstGood xs
+    firstGood (Just n2:xs) = if n2 > n then n2 else firstGood xs
+
+addSquare :: HMap.HashMap (Int, Int) Int -> (Int, Int) -> HMap.HashMap (Int, Int) Int
+addSquare pmap (x0,y0) = HMap.insert (x0,y0) value pmap
+  where
+    value = sum $ map (\k -> HMap.lookupDefault 0 k pmap) neighbors
+    neighbors = [(x0+x,y0+y) | x <- [-1..1], y <- [-1..1], x /= 0 || y /= 0]
+
+spiral :: [(Int, Int)]
+spiral = walk [(1,0)] 1
+  where
+    walk (x:[]) n = x : walk (layer n x) (n+1)
+    walk (x:xs) n = x : walk xs n
+
+layer :: Int -> (Int, Int) -> [(Int, Int)]
+layer n prev = r & d & l & u prev
+  where
+    u (x,y) = [(x,y+t) | t <- [1..2*n-1]]
+    l (x,y) = [(x-t,y) | t <- [1..2*n  ]]
+    d (x,y) = [(x,y-t) | t <- [1..2*n  ]]
+    r (x,y) = [(x+t,y) | t <- [1..2*n+1]]
+
+    infixr 0 &
+    (&) :: (t -> [t]) -> [t] -> [t]
+    (&) dir out = out ++ dir (last out)
