6af894e14e5fe38cd1681ecc0ae47a0be5363891
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-04 08:48:40 -0500

    Remove unused imports & random space in a line

--- haskell/src/y2017/Day04.hs
+++ haskell/src/y2017/Day04.hs
@@ -3,24 +3,13 @@ module Day04 where
 
 import qualified Data.Text.IO as TIO
 import qualified Data.Text    as T
-import           Data.Text    (Text)
 
-import           Data.Char
 import           Data.List
-import           Data.Maybe (catMaybes)
-
-import           Text.Megaparsec
-import           Text.Megaparsec.Text (Parser)
-import qualified Text.Megaparsec.Lexer as L
-
-
-row :: Parser [Integer]
-row = sepBy L.integer space
 
 main :: IO ()
 main = do
   input <- TIO.readFile "src/y2017/input04"
-  let processed =  map (T.splitOn " ") $ T.lines input
+  let processed = map (T.splitOn " ") $ T.lines input
       p1 = part1 processed
       p2 = length $ filter part2 processed
   TIO.putStrLn $ T.pack $ show p1
