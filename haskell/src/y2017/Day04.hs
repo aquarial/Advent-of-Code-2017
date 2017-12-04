b501f32ec8ae2991b1ff7e89055df59f5770fdb2
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-04 08:50:11 -0500

    Move T.unpack up

--- haskell/src/y2017/Day04.hs
+++ haskell/src/y2017/Day04.hs
@@ -9,7 +9,7 @@ import           Data.List
 main :: IO ()
 main = do
   input <- TIO.readFile "src/y2017/input04"
-  let processed = map (T.splitOn " ") $ T.lines input
+  let processed = map (map T.unpack . T.splitOn " ") $ T.lines input
       p1 = part1 processed
       p2 = length $ filter part2 processed
   TIO.putStrLn $ T.pack $ show p1
@@ -20,4 +20,4 @@ part1 a = length a == length (nub a)
 
 
 part2 [] = True
-part2 (a:as) = not (any (\b -> sort (T.unpack a) == sort(T.unpack b)) as) && part2 as
+part2 (a:as) = not (any (\b -> sort a == sort a) as) && part2 as
