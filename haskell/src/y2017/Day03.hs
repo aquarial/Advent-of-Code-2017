352a928438dcd68b2d7c74bed63632b9a33d9874
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-03 19:40:49 -0500

    Re add main printing

--- haskell/src/y2017/Day03.hs
+++ haskell/src/y2017/Day03.hs
@@ -1,12 +1,13 @@
 module Day03 where
 
 import qualified Data.Text.IO as TIO
+import qualified Data.Text    as T
 
 tprint :: Show a => a -> IO ()
 tprint = TIO.putStrLn . T.pack . show
 
---main = do
---  tprint $ part1 289326
+main = do
+  tprint $ part1 289326
 
 part1 :: Int -> Int
 part1 1 = 0
