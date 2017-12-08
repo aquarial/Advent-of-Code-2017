19a65a16acb8dc8d87d2b071f371b98630276f8b
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-08 00:22:02 -0500

    Part 1

--- haskell/src/y2017/Day08.hs
+++ haskell/src/y2017/Day08.hs
@@ -18,29 +18,44 @@ tprint :: Show a => a -> IO ()
 tprint = TIO.putStrLn . T.pack . show
 
 main = do
-  input <- TIO.readFile "src/y2017/input08"
+  input <- TIO.readFile "src/y2017/testinput08"
   case parse p "testinput08" input of
     Left  err   -> TIO.putStr $ T.pack $ parseErrorPretty err
     Right betterInput -> do
       tprint $ partA betterInput
+  input <- TIO.readFile "src/y2017/input08"
   case parse p "input08" input of
     Left  err   -> TIO.putStr $ T.pack $ parseErrorPretty err
     Right betterInput -> do
       tprint $ partA betterInput
   return ()
 
-p :: Parser [Int]
-p = int `sepBy` char '\t'
--- do inpts <- int
---    replicateM inpts $ line `sepBy` char '\n'
+data Ctrl = Inc | Dec
+
+comparitor :: Text -> (Int -> Int -> Bool)
+comparitor ts | ts == "==" = (==)
+              | ts == "!=" = (/=)
+              | ts == ">"  = (>)
+              | ts == "<"  = (<)
+              | ts == ">=" = (>=)
+              | ts == "<=" = (<=)
+
+p :: Parser [(Text, Int->Int, Text, (Int->Bool))]
+p = line `sepBy` char '\n'
 
 line = do
   name <- word
-  string " ("
+  string " "
+  ctr <- (+) <$ string "inc" <|> (subtract) <$ string "dec"
+  string " "
   size <- int
-  string ")"
-  --deps <- option []
-  pure ()
+  string " if "
+  other <- word
+  string " "
+  compar <- comparitor . T.pack <$> some (oneOf (['<','>','!','=']))
+  string " "
+  det <- int
+  pure (name, ctr size, other, (\x -> x `compar` det))
 
 word :: Parser Text
 word = T.pack <$> some letterChar
@@ -55,4 +70,16 @@ tup1 (a,b,c) = a
 tup2 (a,b,c) = b
 tup3 (a,b,c) = c
 
-partA xs = xs
+partA xs = walk xs (M.empty)
+
+walk ::[(Text, Int->Int, Text, (Int->Bool))] -> M.Map Text Int -> M.Map Text Int
+walk []                               m = m
+walk ((this, change, other, test):xs) m = walk xs (if test valother then M.insert this (change valthis) m else m)
+  where
+    valthis :: Int
+    valthis = case M.lookup this m of
+            Just v -> v
+            Nothing -> 0
+    valother = case M.lookup other m of
+            Just v -> v
+            Nothing -> 0
