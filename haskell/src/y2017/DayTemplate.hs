3d0d686597ab55a7ceda873c44587a40875d61ad
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-07 23:45:05 -0500

    Template for advent problems

---
+++ haskell/src/y2017/DayTemplate.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE OverloadedStrings #-}
+module DayTHEDAY where
+
+import           Data.Text             (Text)
+import qualified Data.Text             as T
+import qualified Data.Text.IO          as TIO
+
+import           Text.Megaparsec
+import qualified Text.Megaparsec.Lexer as L
+import           Text.Megaparsec.Text  (Parser)
+
+import           Data.List
+import qualified Data.Map.Strict       as M
+import qualified Data.Set              as S
+
+tprint :: Show a => a -> IO ()
+tprint = TIO.putStrLn . T.pack . show
+
+main = do
+  input <- TIO.readFile "src/y2017/inputTHEDAY"
+  case parse p "inputTHEDAY" input of
+    Left  err   -> TIO.putStr $ T.pack $ parseErrorPretty err
+    Right betterInput -> do
+      tprint $ partA betterInput
+  return ()
+
+p :: Parser [(Text, Int, [Text])]
+p = program `sepBy` char '\n'
+
+program = do
+  name <- word
+  string " ("
+  size <- int
+  string ")"
+  deps <- option [] supports
+  pure (name, size, deps)
+
+word :: Parser Text
+word = T.pack <$> some letterChar
+
+int :: Parser Int
+int = do
+      change <- option id (negate <$ char '-')
+      fromInteger . change <$> L.integer
+
+supports = string " -> "   *>  (T.pack <$> some letterChar) `sepBy` string ", "
+
+
+tup1 (a,b,c) = a
+tup2 (a,b,c) = b
+tup3 (a,b,c) = c
+
+partA xs = xs
