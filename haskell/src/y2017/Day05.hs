d49f940941bbf694d022cacf8c3ec26be3f0d0a0
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-09 09:24:57 -0500

    Part 1 using mutable vector

--- haskell/src/y2017/Day05.hs
+++ haskell/src/y2017/Day05.hs
@@ -5,7 +5,7 @@ import qualified Data.Text             as T
 import qualified Data.Text.IO          as TIO
 
 import           Data.Vector           (Vector)
-import           Data.Vector.Mutable   (MVector)
+import           Data.Vector.Mutable   (MVector, STVector)
 import qualified Data.Vector           as V
 import qualified Data.Vector.Mutable   as MV
 
@@ -25,8 +25,8 @@ main = do
   case parse p "input05" input of
     Left  err   -> TIO.putStr $ T.pack $ parseErrorPretty err
     Right jumps -> do
-      tprint $ part1 jumps
-      --tprint $ part2 jumps
+      tprint $ part1_mut jumps
+      tprint $ part2 jumps
   return ()
 
 p :: Parser (Vector Int)
@@ -38,24 +38,19 @@ p = V.fromList <$> signedInt `sepBy` char '\n'
       neg . fromInteger <$> L.integer
 
 
--- ???
--- part1_mut vec = walk (V.thaw vec) 0 0
---   where
---     walk :: MVector Int Int -> Int -> t -> t
---     walk vec i acc = undefined
-
-
-part1 :: Vector Int -> Int
-part1 vec = walk vec 0 0
+part1_mut :: Num a => Vector Int -> a
+part1_mut vec = ST.runST $ do v <- V.thaw vec
+                              mwalk v 0 0
   where
-    walk :: Vector Int -> Int -> Int -> Int
-    walk vec i acc = case vec V.!? i of
-                       Nothing -> acc
-                       Just dx -> walk (vec V.// [(i, dx+1)]) (i+dx) (acc+1)
+    mwalk v i acc = if MV.length v <= i
+                    then pure acc
+                    else do x <- MV.read v i
+                            MV.write v i $! x+1
+                            mwalk v (i+x) (acc+1)
 
-{-
-part2 :: [Integer] -> Integer
-part2 = walk change . map fromInteger
+
+part2 :: Vector Int -> Integer
+part2 = walk change . V.toList
   where
     change x = if x < 3 then x+1 else x-1
 
@@ -65,4 +60,3 @@ walk changer list = walkacc 0 [] list
     walkacc acc _  []     = acc
     walkacc acc as (b:bs) | b <= 0 = walkacc (acc+1) ( drop (-b) as )  ( reverse (take (-b) as) ++ [changer b] ++ bs )
     walkacc acc as (b:bs) | b >  0 = walkacc (acc+1) ( reverse (take b ((changer b):bs)) ++ as )  (drop b (b:bs))
--}
