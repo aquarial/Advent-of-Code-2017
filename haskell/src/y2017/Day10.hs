50772bc5083c782c86a5b93677c74e6314bfad6d
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-10 18:36:28 -0500

    Even better part1 to match with part2

--- haskell/src/y2017/Day10.hs
+++ haskell/src/y2017/Day10.hs
@@ -21,28 +21,28 @@ import qualified Data.Map.Strict       as M
 import qualified Data.Set              as S
 
 
-p :: Parser [Int]
-p = (int `sepBy` char ',')
+p1 :: Parser [Int]
+p1 = ((fromInteger <$> L.integer) `sepBy` char ',')
 
-int :: Parser Int
-int = do
-      change <- option id (negate <$ char '-')
-      fromInteger . change <$> L.integer
 
 part1 :: [Int] -> Int
-part1 = product . take 2 . third . rounds 1 (0, 0, [0..255])
-  where
-    third (_,_,x) = x
+part1 = product . take 2 . unwind . rounds 1 (0, 0, [0..255])
 
 rounds :: Int -> (Int, Int, [Int]) -> [Int] -> (Int, Int, [Int])
 rounds 0 state inputs = state
 rounds n state inputs = let newState = walk state inputs
                         in rounds (n-1) newState inputs
 
+unwind :: (Int, t, [a]) -> [a]
+unwind (startPos, _, elems) = cycleL (negate startPos) elems
+
 walk          state              []       = state
-walk (startPos, skip, elems) (input:rest) = walk (startPos+skip+input, skip+1, cycleL skip reversed) rest
+walk (startPos, skip, elems) (input:rest) = simplify $ walk (startPos+skip+input, skip+1, cycleL skip reversed) rest
   where
-    reversed = reverse (take input elems) ++ drop input elems
+    reversed = drop input elems ++ reverse (take input elems)
+
+    simplify (start,skip,elems) = (start`mod`l, skip`mod`l, elems)
+    l = fromIntegral (length elems)
 
 cycleL :: Int -> [a] -> [a]
 cycleL i xs = let il = i `mod` length xs
@@ -60,7 +60,7 @@ cycleL i xs = let il = i `mod` length xs
 main :: IO ()
 main = do
   let input = "63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24"
-  case parse p "inputTHEDAY" input of
+  case parse p1 "inputTHEDAY" input of
     Left  err   -> TIO.putStr $ T.pack $ parseErrorPretty err
     Right betterInput -> do
       tprint $ part1 betterInput
