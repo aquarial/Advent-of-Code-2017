2b8472139f02e5b508cdb73c31c5e114682f5b34
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-11 00:29:58 -0500

    Use folds/scans

--- haskell/src/y2017/Day11.hs
+++ haskell/src/y2017/Day11.hs
@@ -26,20 +26,20 @@ move "n" = N
 word :: Parser Text
 word = T.pack <$> some letterChar
 
-part1 = dist . last . walk (0,0)
+part1 :: [Move] -> Double
+part1 = dist . foldl walk (0,0)
 
-part2 = maximum . map dist . walk (0,0)
+part2 :: [Move] -> Double
+part2 = maximum . map dist . scanl walk (0,0)
 
 dist (a,b) = (abs a + abs (a + b) + abs (b)) / 2
 
-walk (a,b) (S :xs) = (a,b):walk (a,  b+1) xs
-walk (a,b) (SE:xs) = (a,b):walk (a+1,b)   xs
-walk (a,b) (SW:xs) = (a,b):walk (a-1,b+1) xs
-walk (a,b) (N :xs) = (a,b):walk (a  ,b-1) xs
-walk (a,b) (NE:xs) = (a,b):walk (a+1,b-1) xs
-walk (a,b) (NW:xs) = (a,b):walk (a-1,b)   xs
-walk  z      []      = [z]
-
+walk (a,b) S  = (a,  b+1)
+walk (a,b) SE = (a+1,b)
+walk (a,b) SW = (a-1,b+1)
+walk (a,b) N  = (a  ,b-1)
+walk (a,b) NE = (a+1,b-1)
+walk (a,b) NW = (a-1,b)
 
 main :: IO ()
 main = do
