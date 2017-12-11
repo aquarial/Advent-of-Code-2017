a4be0418dd87f6e5267ade4b4ad913c75625985e
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-11 00:24:46 -0500

    Clean up a bit

--- haskell/src/y2017/Day11.hs
+++ haskell/src/y2017/Day11.hs
@@ -1,11 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Day11 where
 
-
-import           Data.ByteString       (ByteString)
-import qualified Data.ByteString       as B
-
-import qualified Data.Bits             as Bits
 import           Data.Text             (Text)
 import qualified Data.Text             as T
 import qualified Data.Text.IO          as TIO
@@ -14,15 +9,11 @@ import           Text.Megaparsec
 import qualified Text.Megaparsec.Lexer as L
 import           Text.Megaparsec.Text  (Parser)
 
-import           Data.List
-import qualified Data.Map.Strict       as M
-import qualified Data.Set              as S
-
+data Move = S | SW | NW | N | NE | SE
 
 p :: Parser [Move]
 p = (move <$> word)`sepBy` char ','
 
-data Move = S | SW | NW | N | NE | SE
 
 move :: Text -> Move
 move "se" = SE
@@ -35,27 +26,20 @@ move "n" = N
 word :: Parser Text
 word = T.pack <$> some letterChar
 
-int :: Parser Int
-int = do
-      change <- option id (negate <$ char '-')
-      fromInteger . change <$> L.integer
+part1 = dist . last . walk (0,0)
 
-partA = maximum . map dist . walk (0,0)
-
-
-walk (a,b) (S:xs) = (a,b):walk (a,b+1) xs
-walk (a,b) (SE:xs) = (a,b):walk (a+1,b) xs
-walk (a,b) (SW:xs) = (a,b):walk (a-1,b+1) xs
-walk (a,b) (N:xs) =  (a,b):walk (a,b-1) xs
-walk (a,b) (NE:xs) = (a,b):walk (a+1,b-1) xs
-walk (a,b) (NW:xs) = (a,b):walk (a-1,b) xs
-walk  z      []      = [z]
+part2 = maximum . map dist . walk (0,0)
 
 dist (a,b) = (abs a + abs (a + b) + abs (b)) / 2
 
-test input = case parse p "test" input of
-               Left  err -> TIO.putStr $ T.pack $ parseErrorPretty err
-               Right bi  -> tprint $ partA bi
+walk (a,b) (S :xs) = (a,b):walk (a,  b+1) xs
+walk (a,b) (SE:xs) = (a,b):walk (a+1,b)   xs
+walk (a,b) (SW:xs) = (a,b):walk (a-1,b+1) xs
+walk (a,b) (N :xs) = (a,b):walk (a  ,b-1) xs
+walk (a,b) (NE:xs) = (a,b):walk (a+1,b-1) xs
+walk (a,b) (NW:xs) = (a,b):walk (a-1,b)   xs
+walk  z      []      = [z]
+
 
 main :: IO ()
 main = do
@@ -63,7 +47,8 @@ main = do
   case parse p "input11" input of
     Left  err   -> TIO.putStr $ T.pack $ parseErrorPretty err
     Right betterInput -> do
-      tprint $ partA betterInput
+      tprint $ part1 betterInput
+      tprint $ part2 betterInput
   return ()
 
 
