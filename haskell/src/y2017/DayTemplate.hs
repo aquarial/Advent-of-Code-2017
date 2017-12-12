d7aef1ac104ea060837a7243daa1dc349d52449a
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-12 01:51:16 -0500

    Update template

--- haskell/src/y2017/DayTemplate.hs
+++ haskell/src/y2017/DayTemplate.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
-module DayTHEDAY where
+module DayDAY where
 
 import           Data.Text             (Text)
 import qualified Data.Text             as T
@@ -11,49 +11,33 @@ import           Text.Megaparsec.Text  (Parser)
 
 import           Data.List
 import qualified Data.Map.Strict       as M
-import qualified Data.Set              as S
-
+import qualified Data.HashSet          as S
+import qualified Data.Graph            as G
 
 p :: Parser [()]
 p = line `sepBy` char '\n'
 
-line = do
-  name <- word
-  string " ("
-  size <- int
-  string ")"
-  --deps <- option []
-  pure ()
+line = do a <- int
+          string " - "
+          p <- int `sepBy` string ", "
+          return ()
 
 word :: Parser Text
 word = T.pack <$> some letterChar
 
 int :: Parser Int
-int = do
-      change <- option id (negate <$ char '-')
-      fromInteger . change <$> L.integer
+int = do change <- option id (negate <$ char '-')
+         fromInteger . change <$> L.integer
 
-
-partA xs = xs
-
-
-
+part1 g = g
 
 main :: IO ()
 main = do
-  input <- TIO.readFile "src/y2017/inputTHEDAY"
-  case parse p "inputTHEDAY" input of
-    Left  err   -> TIO.putStr $ T.pack $ parseErrorPretty err
-    Right betterInput -> do
-      tprint $ partA betterInput
-  return ()
-
-
-test :: Text -> IO ()
-test input = case parse p "test" input of
-               Left  err -> TIO.putStr $ T.pack $ parseErrorPretty err
-               Right bi  -> tprint $ partA bi
-
+  input <- TIO.readFile "src/y2017/inputDAY"
+  case parse p "inputDAY" input of
+    Left err -> TIO.putStr $ T.pack $ parseErrorPretty err
+    Right bi -> do
+      tprint $ part1 bi
 
 tprint :: Show a => a -> IO ()
 tprint = TIO.putStrLn . T.pack . show
