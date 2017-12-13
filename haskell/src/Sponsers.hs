22c1876a1eed411b197ed4e4d792ddfd25f26c55
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-13 16:24:39 -0500

    Working on sponser puzzles

--- haskell/src/Sponsers.hs
+++ haskell/src/Sponsers.hs
@@ -6,26 +6,34 @@ import           Data.ByteString        (ByteString)
 import qualified Data.ByteString        as B
 import qualified Data.ByteString.Char8  as C8
 import qualified Data.ByteString.Base64 as B64
+import qualified Data.ByteString.Base16 as B16
 
 import           Data.Text                 (Text)
 import qualified Data.Text                 as T
+import qualified Data.Text.IO              as TIO
 import qualified Data.Text.Encoding        as TE
+import qualified Data.Text.Encoding.Error  as TEerror
 
-smartyStreets :: Either String ByteString
-smartyStreets = B64.decode "U2VuZGluZyBDaHJpc3RtYXMgY2FyZHMgdG8gYmFkIGFkZHJlc3Nlcz8K"
+import Data.Char
 
+fromRight x = case x of
+                Left e -> error e
+                Right xs -> xs
 
--- ??? I'm stuck here
-cheppers = case B64.decode "Pz0pQUI7ChcmER8YDAEYAh4LGwEP" of
-             Left e    -> error e
-             Right msg -> B.pack $ B.zipWith xor msg "↑↑↓↓←→←→BA"
+smartyStreets = fromRight $ B64.decode "U2VuZGluZyBDaHJpc3RtYXMgY2FyZHMgdG8gYmFkIGFkZHJlc3Nlcz8K"
 
--- B64.decode "Pz0pQUI7ChcmER8YDAEYAh4LGwEP"
-a :: ByteString
-a = TE.encodeUtf8 "?=)AB;\n\ETB&\DC1\US\CAN\f\SOH\CAN\STX\RS\v\ESC\SOH\SI"
+cheppers = error "not yet"
 
-b :: ByteString
-b = TE.encodeUtf8 "↑↑↓↓←→←→BA"
+xorB a1 a2 = B.pack $ B.zipWith xor a1 (B.concat [a2,a2,a2,a2,a2])
 
-c :: ByteString
-c = B.pack $ B.zipWith xor a (B.concat [b, b])
+d = TE.decodeUtf8 $ fromRight $ B64.decode "4oqVKEhnZ0pBZ3RUUkVBZUZCOEtDQjBMQjBNWkNSVkJNd3dSSXg4Y0VBb0hIUXdkQXgwSENnPT0sIOKGkeKGkeKGk+KGk+KGkOKGkuKGkOKGkkJBKQ=="
+
+showd = TIO.putStrLn d
+-- ⊕(HggJAgtTREAeFB8KCB0LB0MZCRVBMwwRIx8cEAoHHQwdAx0HCg==, ↑↑↓↓←→←→BA)
+
+hggthing = fromRight $ B64.decode "HggJAgtTREAeFB8KCB0LB0MZCRVBMwwRIx8cEAoHHQwdAx0HCg=="
+
+rot13 x = chr $ (ord x - ord 'a' + ord 'u' - ord 'h') `mod`26 + ord 'a'
+
+workingonit = C8.map (\x -> if isAlpha x then rot13 x else x) $ xorB hggthing "konami"
+
