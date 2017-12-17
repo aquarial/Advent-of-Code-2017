9386dc7f0a003392fa8ce6dc206b84a7d8a471a3
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-17 17:57:58 -0500

    Part 2

--- haskell/src/Y2017/Day17.hs
+++ haskell/src/Y2017/Day17.hs
@@ -14,16 +14,48 @@ import qualified Data.Map.Strict       as M
 import qualified Data.HashSet          as S
 import qualified Data.Graph            as G
 
-parta dist = head $ head $ drop 2017 $ walk 0 dist []
+parta d =  get 0 0 $ generator 1 0 d 1
 
-walk n dx xs = step : walk (n+1) dx step
+get total val ((value,count):xs) = if (total+count > 50*10^6) then val else get (total+count) (value) xs
+
+generator count position distance iters = if position == 0
+                                          then (iters,count) : generator 1 nextpos distance (iters+1)
+                                          else                 generator (count+1) nextpos distance (iters+1)
   where
-    step = drop dxm xs ++ take dxm xs ++ [n]
-    dxm = dx `mod` (if n == 0 then 1 else n)
+    nextpos = ((1+position+distance)`mod` (iters+1))
+{-
+take 100 $ map (\x -> (head x, length x)) $  group $ parta 394
+[(0,1),(1,2),(3,5),(8,15),(23,33),(56,109),(165,786),(951,959),(1910,125),(2035,682),(2717,  C-c C-cInterrupted.
 
+c1 p0 d3 i2
+
+0 (2) 1
+c1 p1 d3 i3
+
+0 2 (3) 1
+c2 p1 d3 i4
+
+0 2 (4) 3 1
+c3 p0 d3 i5
+
+0 (5) 2  4  3  1
+c1 p3 d3 i6
+
+Right (1,0,3,2)
+Right (1,0,3,3)
+Left (1,1,3,4)
+Right (2,0,3,5)
+Left (1,4,3,6)
+Left (2,1,3,7),
+Left (3,4,3,8),
+Left (4,7,3,9),
+Left (5,1,3,10),
+Left (6,4,3,11)]
+-}
 main :: IO ()
 main = do
-  tprint $ parta 394
+  --tprint $ parta 394
+  return ()
 
 tprint :: Show a => a -> IO ()
 tprint = TIO.putStrLn . T.pack . show
