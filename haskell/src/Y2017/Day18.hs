e93c535cf0284bfdca1945b4c8781c413521d913
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-18 17:48:36 -0600

    Finish part 2

--- haskell/src/Y2017/Day18.hs
+++ haskell/src/Y2017/Day18.hs
@@ -16,20 +16,33 @@ import qualified Data.HashSet          as S
 import qualified Data.Graph            as G
 import qualified Data.Vector           as V
 
-parta instrs = walk M.empty 0 0
+parta instrs = let p0 = program 0 instrs p1
+                   p1 = program 1 instrs p0
+               in length $ filter isSend p1
+
+data Network = Send Int | Recieve deriving Show
+
+isSend (Send i) = True
+isSend _        = False
+
+
+program :: Int -> V.Vector Instr -> [Network] -> [Network]
+program name instrs = walk (M.fromList [('p',name)]) 0
   where
-    walk vars recs i = case instrs V.! i of
-                         Snd a   -> walk vars (value vars (Reg a))  (i+1)
-                         Set a b -> walk (M.insert a (value vars b) vars)        recs (i+1)
-                         Add a b -> walk (M.adjust (+    (value vars b)) a vars) recs (i+1)
-                         Mod a b -> walk (M.adjust (`mod`(value vars b)) a vars) recs (i+1)
-                         Mul a b -> walk (M.adjust (*    (value vars b)) a vars) recs (i+1)
-                         Rcv a   -> if value vars (Reg a) /= 0 then recs else walk vars recs (i+1)
-                         Jgz a b -> let test = value vars a
-                                        jump = value vars b
-                                    in if test > 0
-                                       then walk vars recs (i+jump)
-                                       else walk vars recs (i+1)
+    walk vars i inputs = if i >= V.length instrs
+                         then []
+                         else
+                           case instrs V.! i of
+                             Snd a   -> Send (value vars a) : walk vars (i+1) (takeWhile isSend inputs ++ tail (dropWhile isSend inputs))
+                             Set a b -> walk (M.insert a (value vars b) vars)        (i+1) inputs
+                             Add a b -> walk (M.adjust (+    (value vars b)) a vars) (i+1) inputs
+                             Mod a b -> walk (M.adjust (`mod`(value vars b)) a vars) (i+1) inputs
+                             Mul a b -> walk (M.adjust (*    (value vars b)) a vars) (i+1) inputs
+                             Jgz a b -> if value vars a > 0 then walk vars (i + value vars b) inputs else walk vars (i+1) inputs
+                             Rcv a   -> Recieve : case inputs of
+                                                    (Recieve) :rest -> []
+                                                    (Send val):rest -> walk (M.insert a val vars) (i+1) rest
+                                                    _               -> []
 
 value :: M.Map Char Int -> Val -> Int
 value m (Reg c) = M.findWithDefault 0 c m
@@ -37,14 +50,14 @@ value m (Number n) = n
 
 data Val = Reg Char | Number Int deriving Show
 
-data Instr = Snd Char | Set Char Val | Add Char Val | Mul Char Val | Mod Char Val | Rcv Char | Jgz Val Val
+data Instr = Snd Val | Set Char Val | Add Char Val | Mul Char Val | Mod Char Val | Rcv Char | Jgz Val Val
   deriving Show
 
 
 p :: Parser (V.Vector Instr)
 p = V.fromList <$> (parseinstr `sepEndBy` char '\n')
 
-parseinstr = Snd <$> (string "snd " *> letterChar) <|>
+parseinstr = Snd <$> (string "snd " *> pval) <|>
              Set <$> (string "set " *> letterChar) <*> (space *> pval) <|>
              Add <$> (string "add " *> letterChar) <*> (space *> pval) <|>
              Mul <$> (string "mul " *> letterChar) <*> (space *> pval) <|>
