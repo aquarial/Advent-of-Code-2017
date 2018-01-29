e948788cea447a375b0bbea8b3661f8e40fc7c34
Karl <37496339+aquarial@users.noreply.github.com>
2018-01-29 17:24:01 -0500

    Optimize 17b

--- haskell/src/Y2017/Day17.hs
+++ haskell/src/Y2017/Day17.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BangPatterns #-}
+
 module Y2017.Day17 where
 
 import           Data.Text             (Text)
@@ -7,19 +9,27 @@ import qualified Data.Text.IO          as TIO
 
 import           Data.List
 
-parta d = get 0 0 $ generator 1 0 d 1
+partb :: Int -> Int
+partb !d = get 0 0 $ generator 1 0 d 1
 
-get total val ((value,count):xs) = if (total+count > 50*10^6) then val else get (total+count) (value) xs
+get :: Int -> Int -> [(Int, Int)] -> Int
+get !total !val ((!value,!count):xs) =
+  if (total+count > 50*10^6)
+  then val
+  else get (total+count) (value) xs
 
-generator count position distance iters = if position == 0
-                                          then (iters,count) : generator         1 nextpos distance (iters+1)
-                                          else                 generator (count+1) nextpos distance (iters+1)
+generator ::  Int -> Int -> Int -> Int -> [(Int, Int)]
+generator !count !position !distance !iters =
+  if position == 0
+  then (iters,count) : generator         1 nextpos distance (iters+1)
+  else                 generator (count+1) nextpos distance (iters+1)
+
   where
     nextpos = ((1+position+distance)`mod` (iters+1))
 
 main :: IO ()
 main = do
-  --tprint $ parta 394
+  tprint $ partb 394
   return ()
 
 tprint :: Show a => a -> IO ()
