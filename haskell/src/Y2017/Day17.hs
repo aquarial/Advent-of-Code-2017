c861cd6777341fea7b5fb63cfd7a46c2320ada25
Karl <37496339+aquarial@users.noreply.github.com>
2018-01-29 19:55:07 -0500

    Rename for clarity

--- haskell/src/Y2017/Day17.hs
+++ haskell/src/Y2017/Day17.hs
@@ -9,19 +9,19 @@ import qualified Data.Text.IO          as TIO
 import           Data.List
 
 partb :: Int -> Int
-partb d = get 0 0 $ generator (1,1) 0 d
+partb d = millionthInsert 0 0 $ genAfter0Pairs (1,1) 0 d
 
-get :: Int -> Int -> [(Int, Int)] -> Int
-get !inserts !lastVal ((!valAfter0,!untilReplaced):xs) =
+millionthInsert :: Int -> Int -> [(Int, Int)] -> Int
+millionthInsert !inserts !lastVal ((!valAfter0,!untilReplaced):xs) =
   if (inserts+untilReplaced > 50*10^6)
   then lastVal
-  else get (inserts+untilReplaced) (valAfter0) xs
+  else millionthInsert (inserts+untilReplaced) (valAfter0) xs
 
-generator ::  (Int, Int) -> Int -> Int -> [(Int, Int)]
-generator (!count, !iters) !pos !distance =
+genAfter0Pairs ::  (Int, Int) -> Int -> Int -> [(Int, Int)]
+genAfter0Pairs (!count, !iters) !pos !distance =
   if pos == 0
-  then (iters,count) : generator (      1,iters+1) nextpos distance
-  else                 generator (count+1,iters+1) nextpos distance
+  then (iters,count) : genAfter0Pairs (      1,iters+1) nextpos distance
+  else                 genAfter0Pairs (count+1,iters+1) nextpos distance
 
   where
     nextpos = ((1+pos+distance)`mod` (iters+1))
