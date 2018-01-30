a76132ddf34d872d372c271b7df9cc0c954f4599
Karl <37496339+aquarial@users.noreply.github.com>
2018-01-29 23:17:57 -0500

    Finish off 17

--- haskell/src/Y2017/Day17.hs
+++ haskell/src/Y2017/Day17.hs
@@ -6,14 +6,23 @@ import           Data.Text             (Text)
 import qualified Data.Text             as T
 import qualified Data.Text.IO          as TIO
 
+import           Data.Maybe            (catMaybes)
 import           Data.List
 import qualified Data.Sequence         as Seq
 
-parta d = take 10 $ walk d 1 (Seq.fromList [0])
+parta :: Int -> Int
+parta d = head $ catMaybes $ map after2017 $ drop 2017 $ walk d 1 0 (Seq.fromList [0])
 
---walk :: Int -> Int -> Seq.Seq Int -> [Maybe Int]
-walk d v s = let d2 = (d+1) `mod` (Seq.length s)
-             in (s,d2) : walk d (v+1) (v Seq.<| Seq.drop d2 s  Seq.>< (Seq.take d2 s) )
+after2017 :: (Num a, Eq a) => Seq.Seq a -> Maybe a
+after2017 s = case Seq.findIndexL (==2017) s of
+                Nothing -> Nothing
+                Just i -> s Seq.!? (i+1)
+
+walk :: Num a => Int -> a -> Int -> Seq.Seq a -> [Seq.Seq a]
+walk d v i s = s : walk d (v+1) i2 (Seq.insertAt (i+1) v s)
+  where
+    i2 = (i+d+1) `mod` (Seq.length s + 1)
+
 
 partb :: Int -> Int
 partb d = millionthInsert 0 0 $ genAfter0Pairs (1,1) 0 d
@@ -35,7 +44,7 @@ genAfter0Pairs (!count, !iters) !pos !distance =
 
 main :: IO ()
 main = do
-  tprint $ parta 3
+  tprint $ parta 394
   tprint $ partb 394
 
 tprint :: Show a => a -> IO ()
