a52f9b16b53d97bfaebd71b75bc3107f1bcf8c48
Karl <37496339+aquarial@users.noreply.github.com>
2018-02-06 00:00:33 -0500

    Use criterion to benchmark solutions

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,8 @@
 module Main (main) where
 
+
+import Criterion.Main
+
 import qualified Y2017.Day01 as D01
 import qualified Y2017.Day02 as D02
 import qualified Y2017.Day03 as D03
@@ -27,29 +30,18 @@ import qualified Y2017.Day24 as D24
 import qualified Y2017.Day25 as D25
 
 main :: IO ()
-main = do
-  D01.main
-  D02.main
-  D03.main
-  D04.main
-  D05.main
-  D06.main
-  D07.main
-  D08.main
-  D09.main
-  D10.main
-  D11.main
-  D12.main
-  D13.main
-  D14.main
-  D15.main
-  D16.main
-  D17.main
-  D18.main
-  D19.main
-  D20.main
-  D21.main
-  D22.main
-  D23.main
-  D24.main
-  D25.main
+main = benchmark
+
+benchmark :: IO ()
+benchmark = defaultMain [ bgroup "days" [ bench (day n) $ nfIO f | (n,f) <- zip [1..25] allmains] ]
+
+day :: Int -> [Char]
+day n | n <  10 = "0" ++ show n
+      | n >= 10 = show n
+
+allmains :: [IO ()]
+allmains = [D01.main, D02.main, D03.main, D04.main, D05.main,
+            D06.main, D07.main, D08.main, D09.main, D10.main,
+            D11.main, D12.main, D13.main, D14.main, D15.main,
+            D16.main, D17.main, D18.main, D19.main, D20.main,
+            D21.main, D22.main, D23.main, D24.main, D25.main]
