a721c4f5d8df2ca86103259b3f807ad270e4b4d2
Karl <37496339+aquarial@users.noreply.github.com>
2018-02-06 13:23:06 -0500

    Add type signatures and simplify defs. No speedup

--- src/Y2017/Day21.hs
+++ src/Y2017/Day21.hs
@@ -13,7 +13,7 @@ import qualified Text.Megaparsec.Char.Lexer  as L
 import           Data.List
 
 start :: [[Light]]
-start = case parse ppat "start" ".#./..#/###" of
+start = case parse ppat "" ".#./..#/###" of
           Left e -> error "formatting"
           Right x -> x
 
@@ -27,13 +27,15 @@ lightson n xs = numLights . head . drop n $ iterate (iteration nrules) start
     nrules = concat [ zip (allChanges rule) (repeat result) |  (rule, result) <- xs]
     numLights = length . filter (==On) . concat
 
+iteration :: [Rule] -> Pattern -> Pattern
 iteration rules = joinParts . map (applyRule rules) . breakup
 
+applyRule :: [Rule] -> Pattern -> Pattern
 applyRule rules xs = case find (\r -> fst r == xs) rules of
                        Nothing -> error $ "Couldn't match " ++ show (allChanges xs)
                        Just (rule,result)  -> result
 
-joinParts :: [[[a]]] -> [[a]]
+joinParts :: [[[Light]]] -> Pattern
 joinParts xss = concat $ map createRow $ chunk size xss
   where
     size = round $ sqrt $ fromIntegral $ length xss
@@ -45,32 +47,26 @@ chunk n xs = take n xs : chunk n (drop n xs)
 breakup :: [[a]] -> [[[a]]]
 breakup xs = [blocks xs size x0 y0 | y0 <- [0,size..length xs - size]
                                    , x0 <- [0,size..length xs - size]]
-  where size = 2 + (length xs `mod` 2)
+  where
+    size = case length xs `mod` 2 of
+             0 -> 2
+             1 -> 3
 
 blocks :: [[a]] -> Int -> Int -> Int -> [[a]]
 blocks xs n x0 y0 = [[xs !! (y0+y) !! (x0+x) | x <- [0..n-1]] | y <- [0..n-1]]
 
 allChanges :: Show t => [[t]] -> [[[t]]]
-allChanges xs = [f0 (f1 xs) | f0 <- [flipV, flipH, id]
+allChanges xs = [f0 (f1 xs) | f0 <- [id, flipV]
                             , f1 <- [id, rotate, rotate.rotate, rotate.rotate.rotate] ]
 
-flipV xs = [[xs !! y !! x | x <- [0..length (xs !! 0)-1]] | y <- reverse [0..length xs-1]]
-flipH xs = [[xs !! y !! x | x <- reverse [0..length (xs !! 0)-1]] | y <- [0..length xs-1]]
-rotate xs = case length xs of
-              2 -> rotate2 xs
-              3 -> rotate3 xs
-              n -> error $ concat ["length ", show n, " cannot be rotated in ", show xs]
+flipV = reverse
 
-rotate2 xs = map (map (get xs)) $ [ [(0,1), (0,0)], [(1,1), (1,0)]]
-rotate3 xs = map (map (get xs)) $ [ [(0,2),(0,1),(0,0)]
-                                  , [(1,2),(1,1),(1,0)]
-                                  , [(2,2),(2,1),(2,0)] ]
-
-get xs (x,y) = xs !! y !! x
+rotate = flipV . transpose
 
 data Light = On | Off deriving (Show, Eq)
 type Pattern = [[Light]]
 type Rule = (Pattern, Pattern)
+
 
 type Parser = Parsec Void Text
 
@@ -88,11 +84,6 @@ prow = many plight
 
 plight :: Parser Light
 plight = (Off <$ char '.') <|> (On <$ char '#')
-
-int :: Parser Int
-int = do change <- option id (negate <$ char '-')
-         fromInteger . change <$> L.decimal
-
 
 main :: IO ()
 main = do
