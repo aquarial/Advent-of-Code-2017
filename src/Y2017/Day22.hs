464054d7dc189e6239d6e5c4bc795569eea0ddac
Karl <37496339+aquarial@users.noreply.github.com>
2018-02-07 17:22:56 -0500

    Factor out newpos from a and b

--- src/Y2017/Day22.hs
+++ src/Y2017/Day22.hs
@@ -23,12 +23,12 @@ data Action = Cleaning | Infecting deriving (Show, Eq)
 data Dir = U | L | R | D deriving (Show, Eq)
 data Pos = Pos !(Int,Int) !Dir deriving (Show, Eq)
 
-
 parta :: [[Node]] -> Int
 parta = foldl1' (+) . take (10^4) . walka . setup
 
 partb :: [[Node]] -> Int
 partb = foldl1' (+) . take (10^7) . walkb . setup
+
 
 setup :: [[Node]] -> (Pos, M.Map (Int, Int) Node)
 setup xs = (pos, board)
@@ -41,27 +41,25 @@ setup xs = (pos, board)
 
 data Turn = TurnL | TurnR | Opposite | None
 
-
 walka :: (Pos, M.Map (Int, Int) Node) -> [Int]
 walka (Pos !p !d, !b) = case M.findWithDefault Clean p b of
-                          Clean  -> 1:walka (newpos TurnL, M.insert p Infect b)
-                          Infect -> 0:walka (newpos TurnR, M.insert p Clean  b)
-  where
-    newpos TurnL = Pos (move p (turnleft  d)) (turnleft  d)
-    newpos TurnR = Pos (move p (turnright d)) (turnright d)
+                          Clean  -> 1:walka (newpos p d TurnL, M.insert p Infect b)
+                          Infect -> 0:walka (newpos p d TurnR, M.insert p Clean  b)
 
 walkb :: (Pos, M.Map (Int, Int) Node) -> [Int]
 walkb (Pos !p !d, !b) = case M.findWithDefault Clean p b of
-                          Clean ->    0:walkb (newpos TurnL   , M.insert p Weakened b)
-                          Weakened -> 1:walkb (newpos None    , M.insert p Infect   b)
-                          Infect ->   0:walkb (newpos TurnR   , M.insert p Flagged  b)
-                          Flagged ->  0:walkb (newpos Opposite, M.insert p Clean    b)
-  where
-    newpos TurnL =    Pos (move p (turnleft  d)) (turnleft  d)
-    newpos TurnR =    Pos (move p (turnright d)) (turnright d)
-    newpos None  =    Pos (move p            d)             d
-    newpos Opposite = Pos (move p (opposite  d))  (opposite d)
+                          Clean ->    0:walkb (newpos p d TurnL   , M.insert p Weakened b)
+                          Weakened -> 1:walkb (newpos p d None    , M.insert p Infect   b)
+                          Infect ->   0:walkb (newpos p d TurnR   , M.insert p Flagged  b)
+                          Flagged ->  0:walkb (newpos p d Opposite, M.insert p Clean    b)
 
+newpos :: (Int, Int) -> Dir -> Turn -> Pos
+newpos xy d TurnL =    Pos (move xy (turnleft  d)) (turnleft  d)
+newpos xy d TurnR =    Pos (move xy (turnright d)) (turnright d)
+newpos xy d None  =    Pos (move xy            d)             d
+newpos xy d Opposite = Pos (move xy (opposite  d))  (opposite d)
+
+turnleft :: Dir -> Dir
 turnleft U = L
 turnleft L = D
 turnleft D = R
@@ -69,10 +67,10 @@ turnleft R = U
 turnright = turnleft . turnleft . turnleft
 opposite = turnleft . turnleft
 
-move (!x,!y) !U = (x,y-1)
-move (!x,!y) !D = (x,y+1)
-move (!x,!y) !L = (x-1,y)
-move (!x,!y) !R = (x+1,y)
+move (!x,!y) U = (x,y-1)
+move (!x,!y) D = (x,y+1)
+move (!x,!y) L = (x-1,y)
+move (!x,!y) R = (x+1,y)
 
 type Parser = Parsec Void Text
 
