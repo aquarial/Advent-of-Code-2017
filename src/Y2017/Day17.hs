8259039ac82ddadb75df8df93ce95f2d309d8a2b
Karl <37496339+aquarial@users.noreply.github.com>
2018-02-11 04:08:46 -0500

    Apply hlint suggestions

--- src/Y2017/Day17.hs
+++ src/Y2017/Day17.hs
@@ -29,9 +29,9 @@ partb d = millionthInsert 0 0 $ genAfter0Pairs (1,1) 0 d
 
 millionthInsert :: Int -> Int -> [(Int, Int)] -> Int
 millionthInsert !inserts !lastVal ((!valAfter0,!untilReplaced):xs) =
-  if (inserts+untilReplaced > 50*10^6)
+  if inserts+untilReplaced > 50*10^6
   then lastVal
-  else millionthInsert (inserts+untilReplaced) (valAfter0) xs
+  else millionthInsert (inserts+untilReplaced) valAfter0 xs
 
 genAfter0Pairs ::  (Int, Int) -> Int -> Int -> [(Int, Int)]
 genAfter0Pairs (!count, !iters) !pos !distance =
@@ -40,7 +40,7 @@ genAfter0Pairs (!count, !iters) !pos !distance =
   else                 genAfter0Pairs (count+1,iters+1) nextpos distance
 
   where
-    nextpos = ((1+pos+distance)`mod` (iters+1))
+    nextpos = (1+pos+distance)`mod` (iters+1)
 
 main :: IO ()
 main = do
