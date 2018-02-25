6eabc41f098f4bd59299cdd3cba14de80853fabc
Karl <37496339+aquarial@users.noreply.github.com>
2018-02-25 12:07:56 -0500

    Update 2016 day 1 to modern

--- src/Y2016/Day01.hs
+++ src/Y2016/Day01.hs
@@ -4,10 +4,10 @@ module Y2016.Day01 where
 import           Data.Text.Lazy          (Text)
 import qualified Data.Text.Lazy           as T
 
+import Data.Void
+import           Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
 import           Text.Megaparsec
-import qualified Text.Megaparsec.Lexer as L
---import           Text.Megaparsec.Text  (Parser)
-import           Text.Megaparsec.Text.Lazy (Parser)
 
 import           Data.Hashable
 import qualified Data.HashSet          as Set
@@ -66,15 +66,17 @@ update pos (Instr turn dist) = newPos newDir
 dist :: Position -> Integer
 dist pos = abs (_x pos) + abs (_y pos)
 
+type Parser = Parsec Void Text
+
 instructions :: Parser [Instr]
 instructions = sepBy instr (string ", ")
   where
     instr :: Parser Instr
-    instr = Instr <$> turn <*> L.integer
+    instr = Instr <$> turn <*> L.decimal
 
     turn :: Parser Turn
     turn = (char 'L' *> pure TurnL) <|> (char 'R' *> pure TurnR)
 
 
 input :: Text
-input = "R1, R3, L2, L5, L2, L1, R3, L4, R2, L2, L4, R2, L1, R1, L2, R3, L1, L4, R2, L5, R3, R4, L1, R2, L1, R3, L4, R5, L4, L5, R5, L3, R2, L3, L3, R1, R3, L4, R2, R5, L4, R1, L1, L1, R5, L2, R1, L2, R188, L5, L3, R5, R1, L2, L4, R3, R5, L3, R3, R45, L4, R4, R72, R2, R3, L1, R1, L1, L1, R192, L1, L1, L1, L4, R1, L2, L5, L3, R5, L3, R3, L4, L3, R1, R4, L2, R2, R3, L5, R3, L1, R1, R4, L2, L3, R1, R3, L4, L3, L4, L2, L2, R1, R3, L5, L1, R4, R2, L4, L1, R3, R3, R1, L5, L2, R4, R4, R2, R1, R5, R5, L4, L1, R5, R3, R4, R5, R3, L1, L2, L4, R1, R4, R5, L2, L3, R4, L4, R2, L2, L4, L2, R5, R1, R4, R3, R5, L4, L4, L5, L5, R3, R4, L1, L3, R2, L2, R1, L3, L5, R5, R5, R3, L4, L2, R4, R5, R1, R4, L3, ERR"
+input = "R1, R3, L2, L5, L2, L1, R3, L4, R2, L2, L4, R2, L1, R1, L2, R3, L1, L4, R2, L5, R3, R4, L1, R2, L1, R3, L4, R5, L4, L5, R5, L3, R2, L3, L3, R1, R3, L4, R2, R5, L4, R1, L1, L1, R5, L2, R1, L2, R188, L5, L3, R5, R1, L2, L4, R3, R5, L3, R3, R45, L4, R4, R72, R2, R3, L1, R1, L1, L1, R192, L1, L1, L1, L4, R1, L2, L5, L3, R5, L3, R3, L4, L3, R1, R4, L2, R2, R3, L5, R3, L1, R1, R4, L2, L3, R1, R3, L4, L3, L4, L2, L2, R1, R3, L5, L1, R4, R2, L4, L1, R3, R3, R1, L5, L2, R4, R4, R2, R1, R5, R5, L4, L1, R5, R3, R4, R5, R3, L1, L2, L4, R1, R4, R5, L2, L3, R4, L4, R2, L2, L4, L2, R5, R1, R4, R3, R5, L4, L4, L5, L5, R3, R4, L1, L3, R2, L2, R1, L3, L5, R5, R5, R3, L4, L2, R4, R5, R1, R4, L3"
